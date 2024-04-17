53422cadc4682e773eb4dc8a16a7fafd8978ec35
Valen <veh3546@rit.edu>
2024-04-17 16:45:14 -0400

    faculty abstract added

---
+++ src/faculty_abstract_associations.sql
@@ -0,0 +1,14 @@
+INSERT INTO faculty_abstract_associations (facultyID, abstractID)
+VALUES (102, 1),
+       (103, 2),
+       (104, 3),
+       (105, 4);
+
+       INSERT INTO faculty (userID, firstName, lastName, email, office, department)
+VALUES (101, "James","Habermas", "jrhicsa@rit.edu", 'GOL-2556', 'IS'),
+       (102, "Dan","Dogaard", "dan.bogaard@rit.edu", 'GOL-1001', 'IS'),
+       (103, "Erik","Golen", "efgics@rit.edu", 'GOL-1002', 'IS'),
+       (104, "Andrew","Meneely", "andy.meneely@rit.edu", 'GOL-1003', 'SE'),
+       (105, "Samuel","MalacMalachowsky", "samvse@rit.edu", 'GOL-1004', 'SE'),
+       (106, "Andrew","Dimock", "awdics@rit.edu", 'GOL-1005', 'IS'),
+       (107, "Garret","Arcoraci", "gpavks@rit.edu", 'GOL-1006', 'IS');
