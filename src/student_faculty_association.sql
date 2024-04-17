2fe02b49cce0de1a1d2af5ac1eadb386d6728618
zhi_jun <liangzhijun1216@gmail.com>
2024-04-17 16:46:36 -0400

    student association

---
+++ src/student_faculty_association.sql
@@ -0,0 +1,11 @@
+USE majors;
+
+INSERT INTO student_faculty_associations (studentID, facultyID)
+VALUES  (208 , 101 ),
+        ( 209, 102 ),
+        (210 , 103 ),
+        (211 , 104),
+        ( 212,105 ),
+        (213 ,106 ),
+        (214 , 107);
+ 
